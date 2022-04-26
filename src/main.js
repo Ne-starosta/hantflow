7c62155fd85a471d6aa3af5435c83f461b6ec03e
Nosov Vladislav <vlnosov@ozon.ru>
2022-04-26 22:13:06 +0300

    init project

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyBAMN3o-_O0597RbpT-zGt7Ylk4LpiXJfQ',
+  authDomain: 'hantflow-2022.firebaseapp.com',
+  projectId: 'hantflow-2022',
+  storageBucket: 'hantflow-2022.appspot.com',
+  messagingSenderId: '334792610671',
+  appId: '1:334792610671:web:eee32a7e877a0863b60e9a',
+  measurementId: 'G-EN3M60CDFX'
+}
+
 new Vue({
   router,
   store,
