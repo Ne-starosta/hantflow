8c5c15550414b6289d441c096255f6c935659684
Nosov Vladislav <vlnosov@ozon.ru>
2022-06-09 19:42:28 +0300

    mini-last-fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
       </div>
       <router-link to="/vacancies">Вакансии</router-link>
       <router-link to="/candidates">Кандидаты</router-link>
-      <router-link to="/logs">Логи</router-link>
+      <router-link to="/logs">История</router-link>
     </div>
     <div>
       {{$store.getters.getEmail}}
